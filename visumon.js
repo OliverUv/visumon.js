89b6c95c3085e689a125cfb4cdbe8a7c7aa7f5ca
Oliver Uvman <oliver.uvman@gmail.com>
2013-04-10 18:29:27 +0200

    Import hashtable and doing things

---
+++ visumon.js
@@ -0,0 +1,23 @@
+// Author: Oliver Uvman (OliverUv @ github)
+// Repository: https://github.com/OliverUv/visumon.js
+// License: MIT License
+// Loosely based on http://pmav.eu/stuff/javascript-game-of-life-v3.1.1/
+
+(function() {
+  var visumon = {
+    cellBorders: {
+      width: 5,
+      height: 5,
+      color: '#FFFFFF'
+    },
+
+    cells: {
+      height: 15,
+      width: 15
+    },
+
+    init: function() {
+      // Initialize the applet
+    }
+};
+}());
