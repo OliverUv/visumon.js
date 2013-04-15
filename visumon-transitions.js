46a3e9f3fcb8e8e268c8706ed0eec4423440f6f5
Oliver Uvman <oliver.uvman@gmail.com>
2013-04-16 00:59:07 +0200

    Improved documentation of transitions.

--- visumon-transitions.js
+++ visumon-transitions.js
@@ -1,11 +1,14 @@
 // Build your own state transition machine! Hooray :D
 
-// Dead, inactive cells are in state 0. All cells start in this state.
-// Cells touched by mouse cursor are changed to state 1.
-// Other states and all state behaviour is defined by you.
-// Whenever a cell is changed, its changes propagate to other cells.
+// Each state is represented by an integer.
+// - Dead, inactive cells are in state 0. All cells start in this state.
+// - Cells touched by mouse cursor are changed to state 1.
+// Other states and all state change behaviour is defined by you.
+// Whenever a cell is changed, its changes may propagate to other cells.
 
-// You need to supply an object with two functions, affected and transition.
+// To build a rule set, you need to supply an object with two functions:
+//  - Affected: Helps the system propagate changes.
+//  - Transition: Tells the system if a cell should change, and to what.
 
 // Affected takes a coordinate and expects a list of coordinates to other
 // possibly affected cells. This will be used whenever a cell changes in order
@@ -16,13 +19,14 @@
 //   affected(4,4) -> [[3,3], [3,4], [3,5], [4,3], [4,5], [5,3], [5,4], [5,5]]
 // This can include negative coordinates, though they will be ignored
 
-// Your transition function takes the position of a cell who's surroundings
-// have changed in a way it might care about. It takes that cell's state,
-// and a list with the positions & states of all cells that could affect it.
-// These other cells are called affectors, and are supplied in the exact same
-// order as in the list of your affected function. Your function should return
-// the next state for this cell.
-//
+// Your transition function takes the following arguments:
+//  - The position of the subject cell, the surroundings of which
+//    changed recently. The transition function decides whether this
+//    cell should change to a different state or not.
+//  - A list of positions and states of all cells possibly affecting
+//    the subject. These are supplied in the exact same order as in the
+//    list your Affected function returns for that coordinate.
+// Your function should return the next state for this cell.
 // Cells outside of the grid will always be reported as having state 0.
 
 
