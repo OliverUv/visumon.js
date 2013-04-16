aa3e5e07e2cfcb234ebfa03efbd6dc3ba805c75a
Oliver Uvman <oliver.uvman@gmail.com>
2013-04-17 00:40:50 +0200

    added gruntfile

---
+++ Gruntfile.js
@@ -0,0 +1,13 @@
+module.exports = function(grunt) {
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json');
+    qunit: {
+      files: ['test/**/*.html']
+    },
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-qunit');
+
+  grunt.registerTask('test', ['qunit']);
+  grunt.registerTask('default', ['qunit']);
+};
