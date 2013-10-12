6f5c808a2c8ddd20be0d1d0181e4793a429aa0a2
Oliver Uvman <oliver.uvman@gmail.com>
2013-10-12 04:30:33 +0200

    Update threejs, restructure js imports

--- three/shaders/HorizontalBlurShader.js
+++ three/shaders/HorizontalBlurShader.js
@@ -43,9 +43,15 @@ THREE.HorizontalBlurShader = {
 
 			"vec4 sum = vec4( 0.0 );",
 
-			"sum += texture2D( tDiffuse, vec2( vUv.x - 0.5 * h, vUv.y ) ) * 0.25;",
-			"sum += texture2D( tDiffuse, vec2( vUv.x, vUv.y ) ) * 0.5;",
-			"sum += texture2D( tDiffuse, vec2( vUv.x + 0.5 * h, vUv.y ) ) * 0.25;",
+			"sum += texture2D( tDiffuse, vec2( vUv.x - 4.0 * h, vUv.y ) ) * 0.051;",
+			"sum += texture2D( tDiffuse, vec2( vUv.x - 3.0 * h, vUv.y ) ) * 0.0918;",
+			"sum += texture2D( tDiffuse, vec2( vUv.x - 2.0 * h, vUv.y ) ) * 0.12245;",
+			"sum += texture2D( tDiffuse, vec2( vUv.x - 1.0 * h, vUv.y ) ) * 0.1531;",
+			"sum += texture2D( tDiffuse, vec2( vUv.x, vUv.y ) ) * 0.1633;",
+			"sum += texture2D( tDiffuse, vec2( vUv.x + 1.0 * h, vUv.y ) ) * 0.1531;",
+			"sum += texture2D( tDiffuse, vec2( vUv.x + 2.0 * h, vUv.y ) ) * 0.12245;",
+			"sum += texture2D( tDiffuse, vec2( vUv.x + 3.0 * h, vUv.y ) ) * 0.0918;",
+			"sum += texture2D( tDiffuse, vec2( vUv.x + 4.0 * h, vUv.y ) ) * 0.051;",
 
 			"gl_FragColor = sum;",
 
--- three/shaders/RGBShiftShader.js
+++ three/shaders/RGBShiftShader.js
@@ -46,7 +46,7 @@ THREE.RGBShiftShader = {
 			"vec2 offset = amount * vec2( cos(angle), sin(angle));",
 			"vec4 cr = texture2D(tDiffuse, vUv + offset);",
 			"vec4 cga = texture2D(tDiffuse, vUv);",
-			"vec4 cb = texture2D(tDiffuse, (vUv + offset) * 0.985);",
+			"vec4 cb = texture2D(tDiffuse, vUv - offset);",
 			"gl_FragColor = vec4(cr.r, cga.g, cb.b, cga.a);",
 
 		"}"
